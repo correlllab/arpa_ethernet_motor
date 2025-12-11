371b6f6ee5b2da5f33d3c0cfa6b6b3a6ea80b3f6
Max Conway <maxlconway@gmail.com>
2025-12-11 10:56:27 -0700

    rosify package

--- example_run.sh
+++ example_run.sh
@@ -3,8 +3,9 @@
 #This will turn on the motor at its full speed
 echo -n -e "\x01\xff" | nc -u -w1 128.138.224.6 8888
 
-#This will turn off the motor
-#echo -n -e "\x00\xff" | nc -u -w1 128.138.224.6 888
+sleep 0.5
 
+#This will turn off the motor
+echo -n -e "\x00\xff" | nc -u -w1 128.138.224.6 8888
 
 
